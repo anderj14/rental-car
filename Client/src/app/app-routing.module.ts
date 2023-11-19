6806949f1932d4762b64015472fffd0d26d0912d
anderj14 <andersonfrias001@gmail.com>
2023-11-19 17:07:16 -0400

    updating the Auth and login - register component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -11,13 +11,25 @@ const routes: Routes = [
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
-  { path: 'vehicles', loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule) },
-  { path: 'customers', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
-  { path: 'reservations', loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule) },
-  { 
-    path: 'invoices', 
+  {
+    path: 'vehicles',
     canActivate: [authGuard],
-    loadChildren: () => import('./invoice/invoice.module').then(m => m.InvoiceModule) 
+    loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule)
+  },
+  {
+    path: 'customers',
+    canActivate: [authGuard],
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+  },
+  {
+    path: 'reservations',
+    canActivate: [authGuard],
+    loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule)
+  },
+  {
+    path: 'invoices',
+    canActivate: [authGuard],
+    loadChildren: () => import('./invoice/invoice.module').then(m => m.InvoiceModule)
   },
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' },
