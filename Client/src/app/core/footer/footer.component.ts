354e4143d54df04c21b9b60ffda2ad78a663303d
anderj14 <andersonfrias001@gmail.com>
2024-06-12 16:16:57 -0400

    fixed error with reservation and modified contact ui

--- Client/src/app/core/footer/footer.component.ts
+++ Client/src/app/core/footer/footer.component.ts
@@ -8,38 +8,36 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <footer class="footer">
-  	  <div class="container">
-        <div class="row">
-          <div class="footer-col">
-            <h4>company</h4>
-            <ul>
-              <li><a routerLink="/about-us" target="_blank">about us</a></li>
-              <li><a routerLink="/vehicles" target="_blank">our services</a></li>
-              <li><a routerLink="/contact" target="_blank">contact</a></li>
-            </ul>
-          </div>
-          <!-- <div class="footer-col">
-            <h4>get help</h4>
-            <ul>
-              <li><a href="#" target="_blank">FAQ</a></li>
-              <li><a href="#" target="_blank">returns</a></li>
-              <li><a href="#" target="_blank">order status</a></li>
-              <li><a href="#" target="_blank">payment options</a></li>
-            </ul>
-          </div> -->
-          
-          <div class="footer-col">
-            <h4>follow us</h4>
-            <div class="social-links">
-              <a href="https://www.facebook.com" target="_blank"><i class="bi bi-facebook"></i></a>
-              <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter-x"></i></a>
-              <a href="https://instagram.com/" target="_blank"><i class="bi bi-instagram"></i></a>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="footer-col">
+        <h4>company</h4>
+        <ul>
+          <li><a routerLink="/about-us" target="_blank">about us</a></li>
+          <li><a routerLink="/vehicles" target="_blank">our services</a></li>
+          <li><a routerLink="/contact" target="_blank">contact</a></li>
+        </ul>
+      </div>
+      <!-- <div class="footer-col">
+        <h4>get help</h4>
+        <ul>
+          <li><a href="#" target="_blank">FAQ</a></li>
+          <li><a href="#" target="_blank">returns</a></li>
+          <li><a href="#" target="_blank">order status</a></li>
+          <li><a href="#" target="_blank">payment options</a></li>
+        </ul>
+      </div> -->
+      
+      <div class="footer-col">
+        <h4>follow us</h4>
+        <div class="social-links">
+          <a href="https://www.facebook.com" target="_blank"><i class="bi bi-facebook"></i></a>
+          <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter-x"></i></a>
+          <a href="https://instagram.com/" target="_blank"><i class="bi bi-instagram"></i></a>
         </div>
-  	  </div>
-    </footer>
-
+      </div>
+    </div>
+  </div>
   `,
   styleUrl: './footer.component.scss'
 })
--- Client/src/app/reservation-info/reservation-info-form/reservation-info-form.component.ts
+++ Client/src/app/reservation-info/reservation-info-form/reservation-info-form.component.ts
@@ -28,7 +28,6 @@ export class ReservationInfoFormComponent implements OnInit {
     private adminReservationService: AdminReservationService,
     private sessionStorage: SessionStorageService,
     private router: Router
-
   ) {
     this.reservation = new ReservationFormValues();
   }
@@ -43,51 +42,78 @@ export class ReservationInfoFormComponent implements OnInit {
     }
   }
 
-  updatePrice(event: any) {
-    this.reservation.rentalCost = event;
-  }
+  calculateRentalCost(): void {
+    if (this.reservation.vehicleId && this.reservation.insuranceId && this.reservation.days) {
+      const selectedVehicle = this.vehicles.find(v => v.id === this.reservation.vehicleId);
+      const selectedInsurance = this.insurances.find(i => i.id === this.reservation.insuranceId);
 
-  onSubmit(reservation: ReservationFormValues) {
-    const newReservation = { ...reservation, rentalCost: +reservation.rentalCost };
-    this.adminReservationService.createReservation(newReservation).subscribe((response: any) => {
-      const newReservationId = response.id;
-      console.log(response);
-      this.router.navigate(['/admin-reservation', newReservationId]);
-    });
-  }
+      if (selectedVehicle && selectedInsurance) {
+        const vehicleRentalPrice = selectedVehicle.rentalPrice;
+        const insurancePrice = selectedInsurance.insurancePrice;
+        const totalDays = this.reservation.days;
 
-  calculateDays() {
-    if (this.reservation.startDate && this.reservation.endDate && this.reservation.vehicleId && this.reservation.insuranceId) {
-      const startDate = new Date(this.reservation.startDate);
-      const endDate = new Date(this.reservation.endDate);
-  
-      // Calcular la diferencia en días
-      const differenceInTime = endDate.getTime() - startDate.getTime();
-      const differenceInDays = differenceInTime / (1000 * 3600 * 24);
-  
-      // Obtener el precio del vehículo seleccionado
-      const selectedVehicle = this.vehicles.find(vehicle => vehicle.id === this.reservation.vehicleId);
-      const vehiclePricePerDay = selectedVehicle ? selectedVehicle.rentalPrice : 0;
-  
-      // Obtener el precio del seguro seleccionado
-      const selectedInsurance = this.insurances.find(insurance => insurance.id === this.reservation.insuranceId);
-      const insurancePrice = selectedInsurance ? selectedInsurance.insurancePrice : 0;
-  
-      // Calcular el costo total del alquiler
-      const totalRentalCost = (vehiclePricePerDay * differenceInDays) + insurancePrice;
-  
-      this.reservation.days = differenceInDays;
-      this.reservation.rentalCost = totalRentalCost;
+        const rentalCost = (vehicleRentalPrice + insurancePrice) * totalDays;
+
+        console.log('Rental Cost:', rentalCost);
+
+        this.reservation.rentalCost = rentalCost;
+        console.log('Reservation:', this.reservation);
+      }
     }
   }
 
-  getCustomerNameById(id: number): string {
-    const customer = this.customers.find(c => c.id === id);
-    return customer ? customer.customerName : '';
+  calculateDays(): void {
+    if (this.reservation.startDate && this.reservation.endDate) {
+      const startDate = new Date(this.reservation.startDate);
+      const endDate = new Date(this.reservation.endDate);
+      const differenceInTime = endDate.getTime() - startDate.getTime();
+      const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+      this.reservation.days = differenceInDays;
+
+      this.calculateRentalCost();
+    }
   }
 
-  getVehicleNameById(id: number): string {
-    const vehicle = this.vehicles.find(v => v.id === id);
-    return vehicle ? vehicle.vehicleName : '';
+  validateReservation(reservation: ReservationFormValues): boolean {
+    console.log('Validating reservation:', reservation);
+    const isValid = (
+      reservation.customerId > 0 &&
+      reservation.days > 0 &&
+      reservation.endDate !== '' &&
+      reservation.insuranceId > 0 &&
+      reservation.startDate !== '' &&
+      reservation.vehicleId > 0 &&
+      reservation.rentalCost > 0
+    );
+    if (!isValid) {
+      console.error('Validation failed. Current reservation values:', reservation);
+    }
+    return isValid;
+  }
+
+  onSubmit(form: any): void {
+    if (this.validateReservation(this.reservation)) {
+      const newReservation = {
+        ...this.reservation,
+        rentalCost: parseFloat(this.reservation.rentalCost.toFixed(2)),
+        customerId: this.reservation.customerId,
+        vehicleId: this.reservation.vehicleId
+      };
+
+      console.log('Sending reservation data:', newReservation);
+
+      this.adminReservationService.createReservation(newReservation).subscribe(
+        (response: any) => {
+          const newReservationId = response.id;
+          console.log(response);
+          this.router.navigate(['/admin-reservation', newReservationId]);
+        },
+        (error: any) => {
+          console.error('Error al crear reserva:', error);
+        }
+      );
+    } else {
+      console.error('Validation failed. Required fields are missing.');
+    }
   }
 }
